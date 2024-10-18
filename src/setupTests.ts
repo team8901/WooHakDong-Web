17136118ae8cbbef06fdc583250363eb78d0a28e
salmonco <xkqjsslsek80@naver.com>
2024-10-19 02:14:25 +0900

    Test: Set up vitest

---
+++ src/setupTests.ts
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom/vitest";
+import { afterAll, afterEach, beforeAll } from "vitest";
+
+// Start server before all tests
+beforeAll(() => {});
+
+//  Close server after all tests
+afterAll(() => {});
+
+// Reset handlers after each test `important for test isolation`
+afterEach(() => {});
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react(), tsconfigPaths()],
   server: {
     // proxy: {
@@ -16,4 +17,10 @@ export default defineConfig({
     // },
     port: 3000,
   },
-});
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    include: ['**/?(*.)test.ts?(x)'],
+  },
+}));
