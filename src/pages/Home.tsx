c0e104297d7c87c2c4814a5a6e3f81be625b65eb
salmonco <xkqjsslsek80@naver.com>
2024-10-19 16:17:32 +0900

    Chore: 임시로 우학동 페이지에서 동아리 전용 페이지로 이동할 수 있도록 함

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => {
-  return <h1>우학동에 오신 걸 환영합니다.</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center h-[100vh] justify-center gap-[40px]">
+      <h1 className="font-semiBold text-[2.4rem]">
+        우학동에 오신 걸 환영합니다.
+      </h1>
+      <div className="flex flex-col gap-[10px] items-center">
+        <span>등록된 동아리 목록</span>
+        <div className="grid grid-cols-2 gap-[20px]">
+          <button
+            className="w-[150px] h-[150px] rounded-[20px] shadow-md flex justify-center items-center bg-lightPrimary"
+            onClick={() => navigate("/doit")}
+          >
+            <span className="font-semiBold text-[2rem]">doit</span>
+          </button>
+          <button
+            className="w-[150px] h-[150px] rounded-[20px] shadow-md flex justify-center items-center bg-lightPrimary"
+            onClick={() => navigate("/musicclub")}
+          >
+            <span className="font-semiBold text-[2rem]">음악동아리</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
