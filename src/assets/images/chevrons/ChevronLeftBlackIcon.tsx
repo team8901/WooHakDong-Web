fa920d250fa9d39bafeb43de6af2b69df053441a
salmonco <xkqjsslsek80@naver.com>
2024-10-22 23:35:53 +0900

    Chore: svg의 prop명 수정

--- src/assets/images/chevrons/ChevronLeftBlackIcon.tsx
+++ src/assets/images/chevrons/ChevronLeftBlackIcon.tsx
@@ -1,16 +1,9 @@
-import { SVGProps } from "react";
+import { SVGProps } from 'react';
 
 const ChevronLeftBlackIcon = (props: SVGProps<SVGSVGElement>) => {
   return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <g id="arrow_back_ios_new_24dp_5F6368 1" clip-path="url(#clip0_412_1141)">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g id="arrow_back_ios_new_24dp_5F6368 1" clipPath="url(#clip0_412_1141)">
         <g id="Group">
           <path
             id="Vector"
