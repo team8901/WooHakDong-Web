a9e092aacdffdf3f59b8c9089b14120ae905f86e
salmonco <xkqjsslsek80@naver.com>
2024-10-30 03:48:36 +0900

    [WHD-164] Refactor: 클릭 가능한 요소를 interative인 button으로 수정

--- src/pages/clubItem/components/ListItem.tsx
+++ src/pages/clubItem/components/ListItem.tsx
@@ -20,14 +20,14 @@ const ListItem = ({ item }: Readonly<ListItemProps>) => {
   };
 
   return (
-    <div className="flex cursor-pointer gap-[12px]" onClick={() => handleItemClick(item)}>
+    <button className="flex cursor-pointer gap-[12px]" onClick={() => handleItemClick(item)}>
       <img
         alt="물품"
         src={item.itemPhoto || '/logo.svg'}
         className="h-[72px] w-[72px] rounded-[14px] border border-lightGray"
       />
-      <div className="flex w-full flex-col gap-[4px]">
-        <div className="flex flex-col gap-[2px]">
+      <div className="flex w-full flex-col items-start gap-[4px]">
+        <div className="flex flex-col items-start gap-[2px]">
           <Body2 text={item.itemName} />
           <div className="flex items-center gap-[4px]">
             <Body4 text={ITEM_CATEGORY[item.itemCategory]} className="text-darkGray" />
@@ -48,7 +48,7 @@ const ListItem = ({ item }: Readonly<ListItemProps>) => {
           </div>
         </div>
       </div>
-    </div>
+    </button>
   );
 };
 
