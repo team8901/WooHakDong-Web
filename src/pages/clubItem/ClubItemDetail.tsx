66e3bc3a6fad2d8217ea69a0259a8854527454c6
salmonco <xkqjsslsek80@naver.com>
2024-10-31 21:51:35 +0900

    Design: 물품 상세 화면에서 대여버튼 디자인 수정

--- src/pages/clubItem/ClubItemDetail.tsx
+++ src/pages/clubItem/ClubItemDetail.tsx
@@ -1,7 +1,7 @@
 import AppBar from '@components/AppBar';
 import Body1 from '@components/Body1';
 import Body2 from '@components/Body2';
-import Body3 from '@components/Body3';
+import Button from '@components/Button';
 import Caption2 from '@components/Caption2';
 import Title3 from '@components/Title3';
 import { getClubInfo } from '@libs/api/club';
@@ -36,7 +36,7 @@ const ClubItemDetailPage = () => {
   };
 
   return (
-    <div className="relative h-full pb-[50px] pt-[56px]">
+    <div className="relative h-full pb-[70px] pt-[56px]">
       <div className="absolute left-0 top-0 w-full">
         <AppBar />
       </div>
@@ -46,18 +46,12 @@ const ClubItemDetailPage = () => {
           <img
             alt="물품"
             src={item.itemPhoto || '/logo.svg'}
+            // src={'/logo.svg'}
             className="h-[192px] w-[192px] rounded-[14px] border border-lightGray"
           />
           <div className="flex flex-col items-center gap-[8px]">
             <Body2 text={CLUB_ITEM_CATEGORY[item.itemCategory]} className="text-darkGray" />
             <Title3 text={item.itemName} />
-            <button
-              type="button"
-              className={`flex items-center justify-center rounded-[7px] px-[8px] py-[4px] ${item.itemAvailable ? 'bg-primary text-white' : 'bg-lightGray text-darkGray'}`}
-              onClick={handleBorrow}
-            >
-              <Body3 text="대여 하기" />
-            </button>
           </div>
         </div>
 
@@ -76,6 +70,10 @@ const ClubItemDetailPage = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="absolute bottom-[20px] left-0 w-full px-[20px]">
+        <Button text={item.itemUsing ? '대여 중' : '대여하기'} onClick={handleBorrow} disabled={item.itemUsing} />
       </div>
     </div>
   );
