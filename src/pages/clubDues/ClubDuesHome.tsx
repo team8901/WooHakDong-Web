dd3477e01920af5093606151965e5b06b90c4acc
salmonco <xkqjsslsek80@naver.com>
2024-11-07 03:48:28 +0900

    [WHD-140] Refactor: 바텀시트 훅으로 분리

--- src/pages/clubDues/ClubDuesHome.tsx
+++ src/pages/clubDues/ClubDuesHome.tsx
@@ -1,75 +1,57 @@
 import ChevronBottomGrayIcon from '@assets/images/chevrons/ChevronBottomGrayIcon';
-import CheckCircleIcon from '@assets/images/club/CheckCircleIcon';
 import AppBar from '@components/AppBar';
-import Body2 from '@components/Body2';
 import Body4 from '@components/Body4';
 import Caption2 from '@components/Caption2';
 import ScrollView from '@components/ScrollView';
 import Title1 from '@components/Title1';
-import Title3 from '@components/Title3';
+import useBottomSheet from '@hooks/useBottomSheet';
 import { getClubInfo } from '@libs/api/club';
 import { getClubDues } from '@libs/api/dues';
 import { CLUB_DUES_SORT_OPTIONS } from '@libs/constant/dues';
+import BottomSheet from '@pages/clubDues/components/BottomSheet';
 // import { CLUB_DUES_DATA } from '@libs/constant/dues';
 import ListItem from '@pages/clubDues/components/ListItem';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ClubDuesResponseData } from 'types/dues';
 
 const ClubDuesHomePage = () => {
   const { clubEnglishName } = useParams<{ clubEnglishName: string }>();
   const [duesList, setDuesList] = useState<ClubDuesResponseData[]>([]);
+  const [filteredDuesList, setFilteredDuesList] = useState<ClubDuesResponseData[]>([]);
 
-  const [sortOpen, setSortOpen] = useState(false);
-  const [selectedSort, setSelectedSort] = useState(0);
-  const sortIdx = useRef(-1);
-  const sortBottomSheetRef = useRef<HTMLDivElement>(null);
-
-  const getData = async () => {
-    if (!clubEnglishName) return;
-
-    const { clubId } = await getClubInfo({
-      clubEnglishName,
-    });
-
-    const year = new Date().getFullYear();
-    const month = new Date().getMonth() + 1;
-
-    const { result } = await getClubDues({ clubId, year, month });
-
-    if (CLUB_DUES_SORT_OPTIONS[selectedSort].value === 'ALL') {
-      setDuesList(result);
+  const filterData = () => {
+    if (CLUB_DUES_SORT_OPTIONS[selectedOption].value === 'ALL') {
+      setFilteredDuesList(duesList);
       return;
     }
 
-    const filteredResult = result.filter(
-      (dues) => dues.clubAccountHistoryInOutType === CLUB_DUES_SORT_OPTIONS[selectedSort].value,
+    const filteredResult = duesList.filter(
+      (dues) => dues.clubAccountHistoryInOutType === CLUB_DUES_SORT_OPTIONS[selectedOption].value,
     );
-    setDuesList(filteredResult);
+
+    setFilteredDuesList(filteredResult);
   };
 
-  useEffect(() => {
-    if (sortOpen) return;
-
-    const isSortOn = selectedSort !== sortIdx.current;
-
-    if (isSortOn) {
-      getData();
-      sortIdx.current = selectedSort;
-    }
-  }, [sortOpen]);
+  const { isOpen, selectedOption, bottomSheetRef, setIsOpen, setSelectedOption } = useBottomSheet(filterData);
 
   useEffect(() => {
-    getData();
+    (async () => {
+      if (!clubEnglishName) return;
+
+      const { clubId } = await getClubInfo({
+        clubEnglishName,
+      });
+
+      const year = new Date().getFullYear();
+      const month = new Date().getMonth() + 1;
+
+      const { result } = await getClubDues({ clubId, year, month });
+
+      setDuesList(result);
+      setFilteredDuesList(result);
+    })();
     // setDuesList(CLUB_DUES_DATA);
-
-    if (!sortBottomSheetRef.current) return;
-
-    sortBottomSheetRef.current.addEventListener('click', (e) => {
-      if (e.target === sortBottomSheetRef.current) {
-        setSortOpen(false);
-      }
-    });
   }, []);
 
   return (
@@ -88,44 +70,24 @@ const ClubDuesHomePage = () => {
       <button
         type="button"
         className="flex items-center gap-[4px] px-[20px] pt-[16px]"
-        onClick={() => setSortOpen((prev) => !prev)}
+        onClick={() => setIsOpen((prev) => !prev)}
       >
-        <Body4 text={CLUB_DUES_SORT_OPTIONS[selectedSort].label} className="text-darkGray" />
-        <ChevronBottomGrayIcon className={`transform transition-all ${sortOpen && '-rotate-180'}`} />
+        <Body4 text={CLUB_DUES_SORT_OPTIONS[selectedOption].label} className="text-darkGray" />
+        <ChevronBottomGrayIcon className={`transform transition-all ${isOpen && '-rotate-180'}`} />
       </button>
 
       <ScrollView fadeTop className="flex h-full flex-col gap-[16px] px-[20px] py-[16px]">
-        {duesList.map((dues) => (
+        {filteredDuesList.map((dues) => (
           <ListItem key={dues.clubAccountHistoryId} dues={dues} />
         ))}
       </ScrollView>
 
-      <div
-        ref={sortBottomSheetRef}
-        className={`absolute left-0 top-0 z-20 h-full w-full bg-black/30 ${sortOpen || 'hidden'}`}
-      >
-        <div className="absolute bottom-0 left-0 w-full">
-          <div className="flex flex-col gap-[20px] rounded-t-[14px] bg-white pb-[30px] pt-[20px]">
-            <div className="h-[5px] w-[50px] self-center rounded-[14px] bg-lightGray" />
-            <div className="flex flex-col gap-[20px] px-[20px]">
-              <Title3 text="회비 내역 유형 선택" />
-              <div className="flex flex-col gap-[20px]">
-                {CLUB_DUES_SORT_OPTIONS.map((option) => (
-                  <button
-                    key={option.value}
-                    type="button"
-                    onClick={() => setSelectedSort(option.id)}
-                    className="flex items-center justify-between"
-                  >
-                    <Body2 text={option.label} />
-                    {selectedSort === option.id && <CheckCircleIcon />}
-                  </button>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <BottomSheet
+        isOpen={isOpen}
+        selectedOption={selectedOption}
+        bottomSheetRef={bottomSheetRef}
+        setSelectedOption={setSelectedOption}
+      />
     </div>
   );
 };
