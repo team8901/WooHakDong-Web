33f22b0374ac643d7ab37ffca357a2dcec0571ab
salmonco <xkqjsslsek80@naver.com>
2024-11-16 18:03:42 +0900

    Chore: 인적사항 submit으로 변경

--- src/pages/member/MemberInfoWrite.tsx
+++ src/pages/member/MemberInfoWrite.tsx
@@ -49,13 +49,25 @@ const MemberInfoWritePage = () => {
     setMemberInfo((prev) => ({ ...prev, memberPhoneNumber: input }));
   };
 
-  const handleButtonClick = () => {
+  const handleStudentNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value.replace(/\D/g, ''); // Remove non-digit characters
+    setMemberInfo((prev) => ({ ...prev, memberStudentNumber: input }));
+  };
+
+  const handleButtonClick = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (disabled) return;
+
     navigate(ROUTE.MEMBER_INFO_CONFIRM, { state: memberInfo });
   };
 
   const handleGenderChange = (gender: Gender) => {
     setMemberInfo((prev) => ({ ...prev, memberGender: gender }));
   };
+
+  const disabled =
+    !memberInfo.memberMajor?.trim() || !memberInfo.memberStudentNumber?.trim() || !memberInfo.memberPhoneNumber?.trim();
 
   if (isLoading) return <div>로딩 중...</div>;
   return (
@@ -64,59 +76,53 @@ const MemberInfoWritePage = () => {
         <AppBar />
       </div>
 
-      <ScrollView fadeTop fadeBottom className="flex h-full flex-col gap-[40px]">
-        <Title2 text="회원님의 정보를 알려주세요" />
+      <form onSubmit={handleButtonClick}>
+        <ScrollView fadeTop fadeBottom className="flex h-full flex-col gap-[40px]">
+          <Title2 text="회원님의 정보를 알려주세요" />
 
-        <div className="flex flex-col gap-[20px]">
-          <div className="flex flex-col gap-[12px]">
-            <Caption2 text="기본 정보" />
-            <InputBox text={memberInfo.memberName} />
-            <GenderSelection gender={memberInfo.memberGender} setGender={handleGenderChange} />
-            <Input
-              inputMode="numeric"
-              label="휴대폰 번호"
-              name="memberPhoneNumber"
-              placeholder="휴대폰 번호를 '-' 없이 입력해 주세요"
-              value={formatPhoneNumber(memberInfo.memberPhoneNumber)}
-              onChange={handlePhoneNumberChange}
-            />
-            <InputBox text={memberInfo.memberEmail} />
-          </div>
+          <div className="flex flex-col gap-[20px]">
+            <div className="flex flex-col gap-[12px]">
+              <Caption2 text="기본 정보" />
+              <InputBox text={memberInfo.memberName} />
+              <GenderSelection gender={memberInfo.memberGender} setGender={handleGenderChange} />
+              <Input
+                inputMode="numeric"
+                label="휴대폰 번호"
+                name="memberPhoneNumber"
+                placeholder="휴대폰 번호를 '-' 없이 입력해 주세요"
+                value={formatPhoneNumber(memberInfo.memberPhoneNumber)}
+                onChange={handlePhoneNumberChange}
+              />
+              <InputBox text={memberInfo.memberEmail} />
+            </div>
 
-          <div className="flex flex-col gap-[12px]">
-            <Caption2 text="학교 정보" />
-            <InputBox text={memberInfo.memberSchool} />
-            <Input
-              label="학과"
-              name="memberMajor"
-              placeholder="학과를 입력해 주세요"
-              value={memberInfo.memberMajor}
-              onChange={handleInputChange}
-            />
-            <Input
-              type="number"
-              inputMode="numeric"
-              label="학번"
-              name="memberStudentNumber"
-              placeholder="학번을 입력해 주세요"
-              value={memberInfo.memberStudentNumber}
-              onChange={handleInputChange}
-            />
+            <div className="flex flex-col gap-[12px]">
+              <Caption2 text="학교 정보" />
+              <InputBox text={memberInfo.memberSchool} />
+              <Input
+                label="학과"
+                name="memberMajor"
+                placeholder="학과를 입력해 주세요"
+                value={memberInfo.memberMajor}
+                onChange={handleInputChange}
+              />
+              <Input
+                type="number"
+                inputMode="numeric"
+                label="학번"
+                name="memberStudentNumber"
+                placeholder="학번을 입력해 주세요"
+                value={memberInfo.memberStudentNumber}
+                onChange={handleStudentNumberChange}
+              />
+            </div>
           </div>
+        </ScrollView>
+
+        <div className="absolute bottom-[20px] left-0 w-full px-[20px]">
+          <Button text="다음" disabled={disabled} />
         </div>
-      </ScrollView>
-
-      <div className="absolute bottom-[20px] left-0 w-full px-[20px]">
-        <Button
-          text="다음"
-          onClick={handleButtonClick}
-          disabled={
-            !memberInfo.memberMajor?.trim() ||
-            !memberInfo.memberStudentNumber?.trim() ||
-            !memberInfo.memberPhoneNumber?.trim()
-          }
-        />
-      </div>
+      </form>
     </div>
   );
 };
