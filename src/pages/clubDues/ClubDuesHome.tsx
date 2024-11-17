7c0cb0b236f33e391f63464f413b46915101a405
salmonco <xkqjsslsek80@naver.com>
2024-11-18 02:14:51 +0900

    Refactor: 회비 데이터 캐싱

--- src/pages/clubDues/ClubDuesHome.tsx
+++ src/pages/clubDues/ClubDuesHome.tsx
@@ -7,103 +7,87 @@ import EmptyText from '@components/EmptyText';
 import Title1 from '@components/Title1';
 import { useToast } from '@contexts/ToastContext';
 import useBottomSheet from '@hooks/useBottomSheet';
-import useLoading from '@hooks/useLoading';
-import { getClubInfo } from '@libs/api/club';
-import { getClubAccount, getClubDues } from '@libs/api/dues';
 import { CLUB_DUES_SORT_OPTIONS } from '@libs/constant/dues';
 import { formatDate } from '@libs/util/formatDate';
 import formatMoney from '@libs/util/formatMoney';
 import BottomSheet from '@pages/clubDues/components/BottomSheet';
-// import { CLUB_DUES_DATA } from '@libs/constant/dues';
 import ListItem from '@pages/clubDues/components/ListItem';
 import { useEffect, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import { useParams } from 'react-router-dom';
 import { ClubDuesResponseData } from 'types/dues';
 import CustomPullToRefresh from '@components/PullToRefresh';
+import useGetClubId from '@hooks/club/useGetClubId';
+import useGetClubDues from '@hooks/dues/useGetClubDues';
+import useGetClubAccount from '@hooks/dues/useGetClubAccount';
 
 const ClubDuesHomePage = () => {
   const { clubEnglishName } = useParams<{ clubEnglishName: string }>();
   const [duesList, setDuesList] = useState<ClubDuesResponseData[]>([]);
   const [filteredDuesList, setFilteredDuesList] = useState<ClubDuesResponseData[]>([]);
-  const [accountInfo, setAccountInfo] = useState({
-    clubAccountBankName: '',
-    clubAccountNumber: '',
-    clubAccountLastUpdateDate: '',
-    clubAccountBalance: 0,
-  });
-  const { isLoading, setIsLoading } = useLoading();
   const { setToastMessage } = useToast();
-  const [clubId, setClubId] = useState(0);
+  const {
+    data: clubId,
+    isError: isClubIdError,
+    isLoading: isClubIdLoading,
+  } = useGetClubId({ clubEnglishName: clubEnglishName || '' });
+  const {
+    data: clubDuesData,
+    isError: isClubDuesError,
+    isLoading: isClubDuesLoading,
+    refetch: refetchClubDues,
+  } = useGetClubDues({
+    clubId: clubId || 0,
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+  });
+  const { data: clubAccountData } = useGetClubAccount({ clubId: clubId || 0 });
 
-  const filterData = () => {
-    if (CLUB_DUES_SORT_OPTIONS[selectedOption].value === 'ALL') {
+  const filterData = (duesList: ClubDuesResponseData[]) => {
+    if (!clubDuesData) return;
+
+    const filterValue = CLUB_DUES_SORT_OPTIONS[selectedOption].value;
+
+    if (filterValue === 'ALL') {
       setFilteredDuesList(duesList);
       return;
     }
 
-    const filteredResult = duesList.filter(
-      (dues) => dues.clubAccountHistoryInOutType === CLUB_DUES_SORT_OPTIONS[selectedOption].value,
-    );
-
+    const filteredResult = duesList.filter((dues) => dues.clubAccountHistoryInOutType === filterValue);
     setFilteredDuesList(filteredResult);
   };
 
-  const { isOpen, selectedOption, bottomSheetRef, setIsOpen, setSelectedOption } = useBottomSheet(filterData);
-
-  const getDuesList = async ({ clubId }: { clubId: number }) => {
-    if (!clubId) return;
-
-    const year = new Date().getFullYear();
-    const month = new Date().getMonth() + 1;
-
-    const { result } = await getClubDues({ clubId, year, month });
-
-    setDuesList(result);
-
-    if (CLUB_DUES_SORT_OPTIONS[selectedOption].value === 'ALL') {
-      setFilteredDuesList(result);
-    } else {
-      const filteredResult = duesList.filter(
-        (dues) => dues.clubAccountHistoryInOutType === CLUB_DUES_SORT_OPTIONS[selectedOption].value,
-      );
-
-      setFilteredDuesList(filteredResult);
-    }
-
-    const { clubAccountBankName, clubAccountNumber, clubAccountLastUpdateDate, clubAccountBalance } =
-      await getClubAccount({ clubId });
-    setAccountInfo({ clubAccountBankName, clubAccountNumber, clubAccountLastUpdateDate, clubAccountBalance });
-  };
-
-  useEffect(() => {
-    (async () => {
-      if (!clubEnglishName) return;
-
-      setIsLoading(true);
-      try {
-        const { clubId } = await getClubInfo({
-          clubEnglishName,
-        });
-        setClubId(clubId);
-
-        await getDuesList({ clubId });
-      } catch (error) {
-        console.error(error);
-        setToastMessage(`회비 정보를 불러오는 중 오류가 발생했어요\n${error}`);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-    // setDuesList(CLUB_DUES_DATA);
-    // setFilteredDuesList(CLUB_DUES_DATA);
-  }, []);
+  const { isOpen, selectedOption, bottomSheetRef, setIsOpen, setSelectedOption } = useBottomSheet(() =>
+    filterData(duesList),
+  );
 
   const handleRefresh = async () => {
-    await getDuesList({ clubId });
+    const { data } = await refetchClubDues();
+    if (!data) return;
+
+    const { result } = data;
+    setDuesList(result);
+    filterData(result);
 
     setToastMessage('회비 내역을 갱신했어요');
   };
+
+  useEffect(() => {
+    if (!clubDuesData) return;
+
+    const { result } = clubDuesData;
+
+    setDuesList(result);
+    filterData(result);
+  }, [clubDuesData]);
+
+  useEffect(() => {
+    if (isClubIdError || isClubDuesError) {
+      setToastMessage(`회비 정보를 불러오는 중 오류가 발생했어요`);
+    }
+  }, [isClubIdError, isClubDuesError]);
+
+  const isLoading = isClubIdLoading || isClubDuesLoading;
 
   return (
     <div className="relative h-full pb-[100px] pt-[56px]">
@@ -112,8 +96,8 @@ const ClubDuesHomePage = () => {
       </div>
 
       <div className="flex flex-col items-end gap-[4px] px-[20px] py-[20px] pb-[40px]">
-        <Caption2 text={`${accountInfo.clubAccountBankName} ${accountInfo.clubAccountNumber}`} />
-        <Title1 text={formatMoney(accountInfo.clubAccountBalance)} className="text-[2.8rem] font-extrabold" />
+        <Caption2 text={`${clubAccountData?.clubAccountBankName} ${clubAccountData?.clubAccountNumber}`} />
+        <Title1 text={formatMoney(clubAccountData?.clubAccountBalance ?? 0)} className="text-[2.8rem] font-extrabold" />
       </div>
 
       <div className="flex items-center justify-between px-[20px]">
@@ -123,7 +107,7 @@ const ClubDuesHomePage = () => {
         </button>
         <div className="flex items-center gap-[4px]">
           <Body4
-            text={`${new Date(accountInfo.clubAccountLastUpdateDate).getFullYear()}년 ${formatDate(accountInfo.clubAccountLastUpdateDate)} 기준`}
+            text={`${new Date(clubAccountData?.clubAccountLastUpdateDate ?? '').getFullYear()}년 ${formatDate(clubAccountData?.clubAccountLastUpdateDate ?? '')} 기준`}
             className="text-darkGray"
           />
           <button type="button" onClick={handleRefresh}>
