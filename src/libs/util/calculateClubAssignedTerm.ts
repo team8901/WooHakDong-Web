d86861f1d2d86e4365b5d3d16a57bae12e84eca8
salmonco <xkqjsslsek80@naver.com>
2024-11-25 09:18:22 +0900

    Chore: 분기 계산 로직 변경

--- src/libs/util/calculateClubAssignedTerm.ts
+++ src/libs/util/calculateClubAssignedTerm.ts
@@ -1,13 +1,16 @@
+/*
+2024년 1학기 = 2024년 3월 1일 ~ 8월 31일
+2024년 2학기 = 2024년 9월 1일 ~ 2025년 2월 마지막날
+*/
 const calculateClubAssignedTerm = () => {
   const today = new Date();
   const month = today.getMonth() + 1;
   const year = today.getFullYear();
 
-  if (month >= 7) {
-    return `${year}-07-01`;
-  } else {
+  if (month >= 3 && month <= 8) {
     return `${year}-03-01`;
   }
+  return `${year}-09-01`;
 };
 
 export default calculateClubAssignedTerm;
