dc6fb2cd1a07f7dc0298ecd50f69d1e3fcd783de
salmonco <xkqjsslsek80@naver.com>
2024-11-25 10:28:30 +0900

    Chore: 이미지 파일 업로드 용량 제한

--- src/pages/clubItem/ClubItemDetail.tsx
+++ src/pages/clubItem/ClubItemDetail.tsx
@@ -102,6 +102,7 @@ const ClubItemDetailPage = () => {
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const MAX_IMAGE_LENGTH = 1;
+    const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
     const files = e.target.files;
 
     if (!files) return;
@@ -118,6 +119,12 @@ const ClubItemDetailPage = () => {
     // 파일 업로드 시 모든 파일 (*.*) 선택 방지 위해 이미지 type을 한 번 더 검증
     if (file.type !== 'image/jpeg' && file.type !== 'image/jpg' && file.type !== 'image/png') {
       setToastMessage('JPG 혹은 PNG 확장자의 이미지만 등록 가능해요');
+      return;
+    }
+
+    // 파일 용량 제한 검증
+    if (file.size > MAX_FILE_SIZE) {
+      setToastMessage('이미지 파일 용량은 5MB 이하만 등록 가능해요');
       return;
     }
 
