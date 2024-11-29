6aec80ee80f4edd780fd086510e12b32b61e3cdd
salmonco <xkqjsslsek80@naver.com>
2024-11-29 17:48:46 +0900

    [WHD-281] Chore: 달력에서 년도나 달을 선택할 수 있도록 설정

--- src/pages/clubSchedule/components/CustomCalendar.tsx
+++ src/pages/clubSchedule/components/CustomCalendar.tsx
@@ -24,7 +24,7 @@ const CustomCalendar = ({
       onChange={setSelectedDate}
       showNeighboringMonth={false} // 이전, 이후 달의 날짜는 보이지 않도록 설정
       calendarType="gregory" // 그레고리력 사용. 일요일부터 시작
-      view="month" // 월 단위로 보이도록 설정
+      // view="month" // 월 단위로 보이도록 설정
       prev2Label={null} // 다음 연도로 이동하는 버튼은 보이지 않도록 설정
       next2Label={null} // 이전 연도로 이동하는 버튼은 보이지 않도록 설정
       onActiveStartDateChange={({ activeStartDate }) => {
